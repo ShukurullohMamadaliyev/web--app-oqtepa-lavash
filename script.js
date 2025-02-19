fb674cd760518b1210f55406b7e04010c3d4ab8f
Shukurulloh Mamadaliyev <code_vista@internet.ru>
2025-02-19 21:18:53 +0500

    tugma qo'shildi

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.querySelector(".nav-links").addEventListener("wheel", function(e) {
-    this.scrollLeft += e.deltaY;
-});
+ 
+    document.querySelector(".nav-links").addEventListener("wheel", function(e) {
+        this.scrollLeft += e.deltaY;
+    });
+    
+    document.addEventListener("DOMContentLoaded", function() {
+        const buttons = document.querySelectorAll(".add-to-cart");
+    
+        buttons.forEach(button => {
+            button.addEventListener("click", function() {
+                const productName = this.previousElementSibling.previousElementSibling.textContent;
+                console.log(`${productName} savatchaga qo'shildi!`);
+            });
+        });
+    });
